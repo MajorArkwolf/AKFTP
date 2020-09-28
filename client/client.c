f53fd5debd2e4152247c953c1dc2aa083da081ae
Peter Crabbe <petercrabbe92@gmail.com>
2020-09-28 12:39:50 +0800

    add connection to client

--- client/client.c
+++ client/client.c
@@ -20,7 +20,7 @@
 
 #define PORT "3490" // the port client will be connecting to
 
-int RunClient() {
+int RunClient(int socket) {
     bool shouldContinue = true;
     char message[MESSAGE_SIZE];
     char *tokens[MAX_NUM_TOKENS];
@@ -51,6 +51,54 @@ int RunClient() {
 }
 
 int StartClient(int argc, char **argv) {
-    RunClient();
+    int sockfd, numbytes;
+    char *buf = NULL;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: client hostname\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            perror("client: connect");
+            close(sockfd);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return EXIT_FAILURE;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *) p->ai_addr),
+              s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+    RunClient(sockfd);
+    return 0;
 }
 
--- client/client.h
+++ client/client.h
@@ -4,7 +4,7 @@
  * The main loop of the client program, asks for user input and sends it where it needs to go
  * @return EXIT_SUCCESS or EXIT_FAILURE based on what happens during the runtime the program
  */
-int RunClient();
+int RunClient(int socket);
 
 /**
  * Starts the client program and sorts the command line arguments into an ip address and port number
