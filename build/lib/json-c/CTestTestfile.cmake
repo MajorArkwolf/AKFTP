cb2941ddce42c1902ff16aaab2dbd038880753ac
Peter Crabbe <petercrabbe92@gmail.com>
2020-10-06 21:41:12 +0800

    update documentation

--- build/lib/json-c/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arkwolf/Documents/Coding/Schoolwork/ICT374-Assignment2-AKFTP/lib/json-c
-# Build directory: /Users/arkwolf/Documents/Coding/Schoolwork/ICT374-Assignment2-AKFTP/build/lib/json-c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("apps")
-subdirs("doc")
--- build/lib/json-c/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arkwolf/Documents/Coding/Schoolwork/ICT374-Assignment2-AKFTP/lib/json-c/apps
-# Build directory: /Users/arkwolf/Documents/Coding/Schoolwork/ICT374-Assignment2-AKFTP/build/lib/json-c/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
